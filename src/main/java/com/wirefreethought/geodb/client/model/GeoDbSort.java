e4f6d11b1944855c72765717d07ad1d617dce18f
Michael Mogley <mrrubato@gmail.com>
2026-08-08 14:37:22 -0700

    refactor: use List.of for GeoDbSort fields
    Prefer the Java 9+ immutable list factory over Arrays.asList wrapping.

--- src/main/java/com/wirefreethought/geodb/client/model/GeoDbSort.java
+++ src/main/java/com/wirefreethought/geodb/client/model/GeoDbSort.java
@@ -1,7 +1,5 @@
 package com.wirefreethought.geodb.client.model;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -21,7 +19,7 @@ public class GeoDbSort
     @Builder
     public GeoDbSort(SortField... fields)
     {
-        this.fields = Collections.unmodifiableList(Arrays.asList(fields));
+        this.fields = List.of(fields);
     }
 
     @Getter
